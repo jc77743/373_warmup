53e3ab2a159cd783c708d7fe5dba4e4b5a75bdee
Jay Chopra <jchopra@andrew.cmu.edu>
2014-01-29 21:53:38 -0500

    finished tasks

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,13 @@
 class Task < ActiveRecord::Base
   attr_accessible :active, :name, :points
+
+  has_many :chores
+
+  validates_presence_of :name
+
+  validates_numericality_of :points, :only_integer => true, :greater_than_or_equal_to => 0
+
+  scope :active, where('active = ?', true)
+  scope :alphabetical, order('name')
   
 end
