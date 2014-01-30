80547ca3f8bb17a571eaf5e39c4e75f2d286d499
Jay Chopra <jchopra@andrew.cmu.edu>
2014-01-29 22:50:12 -0500

    finished chores

--- app/models/chore.rb
+++ app/models/chore.rb
@@ -1,4 +1,26 @@
 class Chore < ActiveRecord::Base
 	attr_accessible :child_id, :completed, :due_on, :task_id
 
+	belongs_to :child
+	belongs_to :task
+
+	validates_date :due_on
+
+	scope :by_task, joins(:task).order('name')
+	scope :chronological, order('due_on')
+	scope :active, where('active = ?', true)
+	scope :pending, where('completed = ?', false)
+	scope :done, where('completed = ?', true)
+	scope :upcoming, where('due_on >= ?', Date.today)
+	scope :past, where('due_on < ?', Date.today)
+
+	def status
+		if ( self.completed) 
+			"Completed"
+		else
+			"Pending"
+		end
+	end
+
+
 end
